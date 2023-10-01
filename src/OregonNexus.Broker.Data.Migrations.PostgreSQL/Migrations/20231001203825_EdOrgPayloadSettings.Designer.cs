1762dc80a28923b2592cf425391836caf5c7489f
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-10-01 20:39:36 +0000

    Start payload settings

--- src/OregonNexus.Broker.Data.Migrations.PostgreSQL/Migrations/20231001203825_EdOrgPayloadSettings.Designer.cs
+++ src/OregonNexus.Broker.Data.Migrations.PostgreSQL/Migrations/20231001203825_EdOrgPayloadSettings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OregonNexus.Broker.Data;
@@ -12,9 +13,11 @@ using OregonNexus.Broker.Data;
 namespace OregonNexus.Broker.Data.Migrations.PostgreSQL.Migrations
 {
     [DbContext(typeof(BrokerDbContext))]
-    partial class BrokerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231001203825_EdOrgPayloadSettings")]
+    partial class EdOrgPayloadSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/OregonNexus.Broker.Data.Migrations.PostgreSQL/Migrations/20231001203825_EdOrgPayloadSettings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OregonNexus.Broker.Data.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EdOrgPayloadSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EducationOrganizationPayloadSettings",
+                columns: table => new
+                {
+                    EducationOrganizationPayloadSettingsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EducationOrganizationId = table.Column<Guid>(type: "uuid", nullable: true),
+                    PayloadDirection = table.Column<int>(type: "integer", nullable: true),
+                    Payload = table.Column<string>(type: "text", nullable: false),
+                    Settings = table.Column<JsonDocument>(type: "jsonb", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EducationOrganizationPayloadSettings", x => x.EducationOrganizationPayloadSettingsId);
+                    table.ForeignKey(
+                        name: "FK_EducationOrganizationPayloadSettings_EducationOrganizations~",
+                        column: x => x.EducationOrganizationId,
+                        principalTable: "EducationOrganizations",
+                        principalColumn: "EducationOrganizationId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EducationOrganizationPayloadSettings_EducationOrganizationI~",
+                table: "EducationOrganizationPayloadSettings",
+                columns: new[] { "EducationOrganizationId", "Payload" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EducationOrganizationPayloadSettings");
+        }
+    }
+}
