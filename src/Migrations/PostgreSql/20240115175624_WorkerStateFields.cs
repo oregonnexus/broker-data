f1414a434d69409c17f920313966e86c02a3ab7a
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-01-15 18:22:12 +0000

    Add worker state fields

---
+++ src/Migrations/PostgreSql/20240115175624_WorkerStateFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OregonNexus.Broker.Data.Migrations.PostgreSql
+{
+    /// <inheritdoc />
+    public partial class WorkerStateFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessState",
+                table: "Requests",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkerInstance",
+                table: "Requests",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessState",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "WorkerInstance",
+                table: "Requests");
+        }
+    }
+}
