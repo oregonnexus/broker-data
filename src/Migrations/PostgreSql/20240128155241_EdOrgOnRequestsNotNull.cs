0e2c64b568261e7d70e85f0486d97e964e717518
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-01-29 04:35:09 +0000

    Process sending request

---
+++ src/Migrations/PostgreSql/20240128155241_EdOrgOnRequestsNotNull.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OregonNexus.Broker.Data.Migrations.PostgreSql
+{
+    /// <inheritdoc />
+    public partial class EdOrgOnRequestsNotNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_EducationOrganizations_EducationOrganizationId",
+                table: "Requests");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EducationOrganizationId",
+                table: "Requests",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_EducationOrganizations_EducationOrganizationId",
+                table: "Requests",
+                column: "EducationOrganizationId",
+                principalTable: "EducationOrganizations",
+                principalColumn: "EducationOrganizationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_EducationOrganizations_EducationOrganizationId",
+                table: "Requests");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EducationOrganizationId",
+                table: "Requests",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_EducationOrganizations_EducationOrganizationId",
+                table: "Requests",
+                column: "EducationOrganizationId",
+                principalTable: "EducationOrganizations",
+                principalColumn: "EducationOrganizationId");
+        }
+    }
+}
